9308066d5bd703b3e30750d534757cbe9058119d
Darmawan Salihun <darmawan.salihun@gmail.com>
2016-01-11 15:19:38 +0700

    debugging feature for spsc wrapper library completed

--- spsc_lib/spsc_interface.cc
+++ spsc_lib/spsc_interface.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "spsc_interface.hpp"
 
 spsc_interface::spsc_interface():producer_count(0),
@@ -24,6 +25,7 @@ bool spsc_interface::push_item(const struct account * const acc)
     struct account tmp_acc;
 
     assert(acc != NULL);
+	assert(connected_producer == 1);
 
     memcpy(&tmp_acc, acc, sizeof(struct account));
 
@@ -37,6 +39,7 @@ bool spsc_interface::pop_item(struct account * acc)
     bool retval = false;
 
     assert(acc != NULL);
+	assert(connected_consumer == 1);
 
     retval = spsc_queue.pop(tmp_acc);
     if (retval == false) {
